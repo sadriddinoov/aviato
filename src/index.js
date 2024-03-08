4c143e21d0b2622369dec0850beda7e2dcc7c234
sadriddinoov <bilolsadriddinov941@gmail.com>
2024-03-08 20:11:10 +0500

    lang

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { lang } from './lang/lang';
+
+i18next.use(initReactI18next).init({
+  resources: lang,
+  lng: JSON.parse(localStorage.getItem('lang')) || 'uz'
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/lang/lang.jsx
@@ -0,0 +1,139 @@
+export const lang = {
+    uz: {
+        translation: {
+            header : {
+                cart: `Savat`,
+                search: "Qidirish"
+            },
+
+            navbars: {
+                home: "Uy",
+                shop: `Do'kon`,
+                pages: `Sahifalar`,
+                checkout: `To'lov`,
+                cart: `Savat`,
+                pricing: 'Narxlash',
+                layout: `Tartib`,
+                details: "Mahsulotning tafsilotlari",
+                sidebar: "Yon panel bilan sotib olish",
+                introduction: `Tanishtirish`,
+                contact: `Biz bilan bog'lanish`,
+                about: "Biz haqimizda",
+                404: `404 sahifasi`,
+                soon: "tez kunlarda",
+                faq: "TSS",
+                dashboard: `Asboblar paneli`,
+                interface: `foydalanuvchi interfeysi`,
+                orders: "Buyurtmalar",
+                address: `address`,
+                profile: `profil malutmotlari`,
+                utility: `Qulaylik`,
+                login: `kirish sahifasi`,
+                signin: `royhatdan otish`,
+                forget: `parolni unutish`,
+                blog: `blog`,
+                left: `blog chap panelda`,
+                right: `blog o'ng panelda`,
+                full: `blog to'liq kengligda`,
+                columns: `blog 2 ustunda`,
+                single: `blog yagona bo'lib`,
+                elements: `elementlar`,
+                typography: `tipografia`,
+                buttons: `tugmalar`,
+                alerts: `ogohlantirishlar`
+            }
+        }
+    },
+
+    ru: {
+        translation: {
+            header : {
+                cart: `Корзина`,
+                search: "Искать"
+            },
+
+            navbars: {
+                home: "Дом",
+                shop: `Покупать`,
+                pages: `Страницы`,
+                checkout: `Оплатить`,
+                cart: `Корзина`,
+                pricing: 'Цены',
+                layout: `Структура`,
+                details: "Детали о продуктов",
+                sidebar: "Купить с боковой панелью",
+                introduction: `введение`,
+                contact: `свяжитесь с нами`,
+                about: "о нас",
+                404: `404 страница`,
+                soon: "скоро",
+                faq: "чзв",
+                dashboard: `панель приборов`,
+                interface: `интерфейс клиента`,
+                orders: "заказы",
+                address: `адресс`,
+                profile: `информация о клиенте`,
+                utility: `плюсы`,
+                login: `страница авторизации`,
+                signin: `регистрация`,
+                forget: `забыл пароль`,
+                blog: `влог`,
+                left: `влог в левой панельи`,
+                right: `влог в правой панели`,
+                full: `влог в полной широте`,
+                columns: `влог в 2 столбах`,
+                single: `влог в 1 столбе`,
+                elements: `элемент`,
+                typography: `типография`,
+                buttons: `кнопки`,
+                alerts: `предупреждения`
+            }
+        }
+    },
+
+    eng: {
+        translation: {
+            header : {
+                cart: `Cart`,
+                search: "Search"
+            },
+
+            navbars: {
+                home: "Home",
+                shop: `Shop`,
+                pages: `Pages`,
+                checkout: `Checkout`,
+                cart: `cart`,
+                pricing: 'pricing',
+                layout: `layout`,
+                details: "product details",
+                sidebar: "shop with sidebar",
+                introduction: `introduction`,
+                contact: `contact us`,
+                about: "about us",
+                404: `404 page`,
+                soon: "coming soon",
+                faq: "faq",
+                dashboard: `dashboard`,
+                interface: `user interface`,
+                orders: "orders",
+                address: `address`,
+                profile: `profile details`,
+                utility: `utility`,
+                login: `log in`,
+                signin: `sign in`,
+                forget: `fotget password`,
+                blog: `blog`,
+                left: `blog left sidebar`,
+                right: `blog right sidebar`,
+                full: `blog full width`,
+                columns: `blog 2 columns`,
+                single: `blog single`,
+                elements: `elements`,
+                typography: `typography`,
+                buttons: `buttons`,
+                alerts: `alerts`
+            }
+        }
+    }
+}
